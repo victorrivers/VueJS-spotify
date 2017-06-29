06a671122d77552c6c8d00eb83707226b1a24cf1
Victor Rios <victor_github@outlook.com>
2017-06-30 00:04:12 +0200

    Navigation bar

--- source/public/script/viewmodels.js
+++ source/public/script/viewmodels.js
@@ -2,6 +2,40 @@ var viewmodels = {};
 
 (function(){
 	
+	var viewSelectorVm = new Vue({
+		el: '.main-view-container',
+		data: {
+			currentView: 'featured',
+			menuItems: [
+				{ key: 'featured', name: 'FEATURED' },
+				{ key: 'genres-and-moods', name: 'GENRES & MOODS' },
+				{ key: 'new-releases', name: 'NEW RELEASES' },
+				{ key: 'discover', name: 'DISCOVER' }
+			]
+		},
+		components: {
+			featured: {
+				template: '<div class="view-content">Featured content</div>'
+			},
+			genresAndMoods: {
+				template: '<div class="view-content">GENRES & MOODS!</div>'
+			},
+			newReleases: {
+				template: '<div class="view-content">New relases content</div>'
+			},
+			discover: {
+				template: '<div class="view-content">Discover</div>'
+			}
+		},
+		methods: {
+			updateView: function (view) {
+				if (view !== this.currentView) {
+					this.currentView = view;
+				}
+			}
+		}
+	});
+	
 	viewmodels.newReleasesVm = new Vue({
 		el: '.new-releases-container',
 		data: {
@@ -25,4 +59,5 @@ var viewmodels = {};
 			show: false
 		}
 	});
+	
 })();
